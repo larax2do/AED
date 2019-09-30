0a5288db8995f172248b6dc8532f82a1e5925e11
thales <furion2298@gmail.com>
2019-09-30 13:12:37 -0400

    Se continua en el desarrollo del arbol binario

--- arboles/include/ArbolBinBus.h
+++ arboles/include/ArbolBinBus.h
@@ -2,27 +2,55 @@
 #define ARBOLBINBUS_H
 #include <iostream>
 #include <ostream>
+#include <functional>
 
 
-template <typename Comparable>
+template <typename Objeto, typename Comparador=std::less<Objeto>>
 class ArbolBinBus
 {
     public:
-        ArbolBinBus();//constructor por defecto
-        ArbolBinBus(const ArbolBinBus& rhs);//constructor copia para lvalues
-        ArbolBinBus(ArbolBinBus && rhs);//constructor move para rvalues
-        ~ArbolBinBus();//destructor
+        ArbolBinBus()//constructor por defecto
+        {
+            raiz=nullptr;
+        }
+        ArbolBinBus(const ArbolBinBus& rhs):raiz{nullptr}//constructor copia para lvalues
+        {
+            raiz=clonar(rhs.raiz);
+        }
+        ArbolBinBus(ArbolBinBus && rhs)//constructor move para rvalues
+        {
 
-        const Comparable& buscarMinimo()const;
-        const Comparable& buscarMaximo()const;
-        bool contiene(const Comparable& x)const;
+        }
+        ~ArbolBinBus()//destructor
+        {
+            prepararVacio();
+        }
+
+        const Objeto& buscarMinimo()const;
+        const Objeto& buscarMaximo()const;
+        bool contiene(const Objeto& x)const
+        {
+            return contiene(x,raiz);
+        }
         bool estaVacio()const;
         void mostrarArbol(std::ostream& salida=std::cout)const;
 
-        void prepararVacio();
-        void insertar(const Comparable& x);
-        void insertar(Comparable&& x);
-        void remover(const Comparable& x);
+        void prepararVacio()
+        {
+
+        }
+        void insertar(const Objeto& x)
+        {
+            insertar(x,raiz);
+        }
+        void insertar(Objeto&& x)
+        {
+            insertar(std::move(x),raiz);
+        }
+        void remover(const Objeto& x)
+        {
+            remover(x,raiz);
+        }
 
 
         ArbolBinBus& operator=(const ArbolBinBus& rhs);
@@ -34,17 +62,17 @@ class ArbolBinBus
 
         struct NodoBinario
         {
-            Comparable dato;
+            Objeto dato;
             NodoBinario *izquierda;
             NodoBinario *derecha;
 
-            NodoBinario(const Comparable& elemento, NodoBinario* subIzquierdo, NodoBinario* subDerecho)
+            NodoBinario(const Objeto& elemento, NodoBinario* subIzquierdo, NodoBinario* subDerecho)
             {
                 dato=elemento;
                 izquierda=subIzquierdo;
                 derecha=subDerecho;
             }
-            NodoBinario(Comparable&& elemento, NodoBinario* subIzquierdo, NodoBinario* subDerecho)
+            NodoBinario(Objeto&& elemento, NodoBinario* subIzquierdo, NodoBinario* subDerecho)
             {
                 dato=std::move(elemento);
                 izquierda=subIzquierdo;
@@ -54,19 +82,137 @@ class ArbolBinBus
         };
 
         NodoBinario* raiz;
+        Comparador esMenosQue;
 
-        void insertar(const Comparable& x, NodoBinario*& t);
-        void insertar(Comparable&& x, NodoBinario*& t);
-        void remover(const Comparable& x, NodoBinario*& t);
+        void insertar(const Objeto& x, NodoBinario*& t)
+        {
+            if(t==nullptr)
+            {
+                t=new NodoBinario{x,nullptr,nullptr};
+            }
+            else if(x < t->dato)
+            {
+                insertar(x,t->izquierda);
+            }
+            else if(x > t->dato)
+            {
+                insertar(x,t->derecha);
+            }
+            else
+                ;//si llega aqui significa que el dato ya existe en el arbol
+        }
+        void insertar(Objeto&& x, NodoBinario*& t)
+        {
+            if(t==nullptr)
+            {
+                t=new NodoBinario{std::move(x),nullptr,nullptr};
+            }
+            else if(x < t->dato)
+            {
+                insertar(std::move(x),t->izquierda);
+            }
+            else if(x > t->dato)
+            {
+                insertar(std::move(x),t->derecha);
+            }
+            else
+                ;//si llega aqui significa que el dato ya existe en el arbol
+        }
+        void remover(const Objeto& x, NodoBinario*& t)
+        {
+            if(t==nullptr)
+            {
+                return ; //Recorrio hasta aqui por la wea, ya que no encontro nada
+            }
+            else if(x < t->dato)
+            {
+                remover(x,t->izquierda);
+            }
+            else if(x > t->dato)
+            {
+                remover(x,t->derecha);
+            }
+            else if(t->izquierda!=nullptr && t->derecha!=nullptr)
+            {
+                t->dato=buscarMinimo(t->derecha)->dato;
+                remover(t->dato,t->derecha);
+            }
+            else
+            {
+                NodoBinario* antiguo=t;
+                t=(t->izquierda != nullptr)? t->izquierda:t->derecha;
+                delete antiguo;
+            }
+        }
+        NodoBinario* buscarMinimo(NodoBinario* t)const
+        {
+            if(t==nullptr)
+            {
+                return nullptr;
+            }
+            if(t->izquierda==nullptr)
+            {
+                return t;
+            }
+            return buscarMinimo(t->izquierda);
+        }
+        NodoBinario* buscarMaximo(NodoBinario* t)const
+        {
+            if(t!=nullptr)
+            {
+                while(t->derecha!=nullptr)
+                {
+                    t=t->derecha;
+                }
+            }//end if
+            return t;
+        }
 
-        NodoBinario* buscarMinimo(NodoBinario* t) const;
-        NodoBinario* buscarMaximo(NodoBinario* t)const;
-
-        bool contiene(const Comparable& x,NodoBinario* t)const;
-        void prepararVacio(NodoBinario*& t);
+        bool contiene(const Objeto& x,NodoBinario* t)const
+        {
+            if(t==nullptr)
+            {
+                return false;
+            }
+            else if(esMenosQue(x,t->dato))
+            {
+                return contiene(x,t->izquierda);
+            }
+            else if(esMenosQue(t->dato,x))
+            {
+                return contiene(x,t->derecha);
+            }
+            else
+            {
+                return false;
+            }
+        }
+        void prepararVacio(NodoBinario*& t)
+        {
+            if(t!=nullptr)
+            {
+                prepararVacio(t->izquierda);
+                prepararVacio(t->derecha);
+                delete t;
+            }
+            t=nullptr;
+        }
         void mostrarArbol(NodoBinario* t, std::ostream& salida)const;
-        NodoBinario* clonar(NodoBinario* t)const;
+        NodoBinario* clonar(NodoBinario* t)const
+        {
+            if(t==nullptr)
+            {
+                return nullptr;
+            }
+            else
+            {
+                return new NodoBinario{t->dato,clonar(t->izquierda),clonar(t->derecha)};
+            }
+        }
 };
+
+
+
 
 
 
--- arboles/src/ArbolBinBus.cpp
+++ arboles/src/ArbolBinBus.cpp
@@ -1,43 +1,5 @@
 #include "ArbolBinBus.h"
 
-template <typename Comparable>
-bool ArbolBinBus<Comparable>::contiene(const Comparable& x)const
-{
-    return contiene(x,raiz);
-}
-
-template <typename Comparable>
-void ArbolBinBus<Comparable>::insertar(const Comparable& x)
-{
-    insertar(x,raiz);
-}
-
-template <typename Comparable>
-void ArbolBinBus<Comparable>::remover(const Comparable& x)
-{
-    remover(x,raiz);
-}
-
-template <typename Comparable>
-bool ArbolBinBus<Comparable>::contiene(const Comparable& x,NodoBinario* t) const
-{
-    if(t==nullptr)
-    {
-        return false;
-    }
-    else if(x < t->dato)
-    {
-        return contiene(x,t->izquierda);
-    }
-    else if(x > t->dato)
-    {
-        return contiene(x,t->derecha);
-    }
-    else
-    {
-        return true;
-    }
-}
 /*
 ArbolBinBus::ArbolBinBus()
 {
